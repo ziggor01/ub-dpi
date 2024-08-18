ab67a811dc1a65579d94f46da527d596e2f97dd0
ziggor01 <ziggor01@gmail.com>
2024-08-18 11:51:09 +0300

    1

--- new-ub-post.sh
+++ new-ub-post.sh
@@ -6,6 +6,7 @@ sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
+echo "#################################################################################################"
 # Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
@@ -14,15 +15,17 @@ echo \
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
+echo "#################################################################################################"
 #Налаштування групи для поточного користувача
 sudo usermod -aG docker $USER
 newgrp docker
 sudo groupadd docker
 
+echo "#################################################################################################"
 #тестові дії
 docker run hello-world
 docker run -it --rm -d -p 8080:80 --name nginx nginx
-docker run -it --rm -d -p 8080:80 --name web nginx
+docker run -it --rm -d -p 8081:80 --name web nginx
 docker ps
 docker images
 docker rmi hello-world:latest nginx:latest -f
