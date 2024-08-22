b46cdba22c5b03401b734419da0d3e4e2062d4eb
ziggor01 <ziggor01@gmail.com>
2024-08-22 21:20:05 +0300

    [DZ] add

--- ub-kpi.sh
+++ ub-kpi.sh
@@ -2,4 +2,8 @@
 echo "START"
 #Оновлення + встановлення знідно інструкції на офф сайті
 sudo apt -y update
-sudo apt install mc curl snap
+sudo apt install -y mc curl snap apt-transport-https
+url -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
