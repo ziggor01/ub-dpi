79184ebb413b19ddb9853bcfa4504e8b0660b0c4
ziggor01 <ziggor01@gmail.com>
2024-10-13 20:37:16 +0300

    [DZ] re

--- ub-gitlabpi.sh
+++ ub-gitlabpi.sh
@@ -2,3 +2,8 @@
 echo "START"
 #Оновлення + встановлення знідно інструкції на офф сайті
 sudo apt -y update
+sudo apt install -y curl openssh-server ca-certificates tzdata perl
+sudo apt-get install -y postfix
+
+curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
+
