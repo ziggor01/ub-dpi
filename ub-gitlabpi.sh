dd930b592855e9f6cac4f27d649ab1ec3f7f14dd
ziggor01 <ziggor01@gmail.com>
2024-10-27 21:04:59 +0200

    [DZ] add mc

--- ub-gitlabpi.sh
+++ ub-gitlabpi.sh
@@ -2,9 +2,13 @@
 echo "START"
 #Оновлення + встановлення знідно інструкції на офф сайті
 sudo apt -y update
-sudo apt install -y curl openssh-server ca-certificates tzdata perl
+sudo apt install -y curl openssh-server ca-certificates tzdata perl mc
 sudo apt-get install -y postfix
 
 curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
 
-sudo swapoff -a
+# Встановіть новий пароль для root
+NEW_PASSWORD="Pass12345"
+
+# Скидання пароля
+echo -e "$NEW_PASSWORD\n$NEW_PASSWORD" | sudo gitlab-rake "gitlab:password:reset[root]"
