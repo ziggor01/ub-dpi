9b5db7383daa4c5269c75ed6924a510846786334
ziggor01 <97229142+ziggor01@users.noreply.github.com>
2024-10-27 20:19:04 +0200

    Update ub-gitlabpi.sh

--- ub-gitlabpi.sh
+++ ub-gitlabpi.sh
@@ -6,5 +6,3 @@ sudo apt install -y curl openssh-server ca-certificates tzdata perl
 sudo apt-get install -y postfix
 
 curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
-
-sudo swapoff -a
