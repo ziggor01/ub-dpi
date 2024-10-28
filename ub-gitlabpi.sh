51d079bb990c1b9d8cb8e16e198a42c834fbe26c
ziggor01 <ziggor01@gmail.com>
2024-10-28 20:12:21 +0200

    [DZ] update script

--- ub-gitlabpi.sh
+++ ub-gitlabpi.sh
@@ -5,7 +5,9 @@ sudo apt -y update
 sudo apt install -y curl openssh-server ca-certificates tzdata perl mc
 sudo apt-get install -y postfix
 
+cd /tmp
 curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
+sudo apt install gitlab-ce
 
 # Встановіть новий пароль для root
 NEW_PASSWORD="Pass12345"
