05253bee08dd10770d74b8d7a2c6ef57c85d1f5e
ziggor01 <ziggor01@gmail.com>
2024-11-02 20:36:56 +0200

    [DZ] add check

--- ub-kpi.sh
+++ ub-kpi.sh
@@ -44,3 +44,7 @@ sudo kubeadm init
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+####################################################################
+
+kubectl get pods -n kube-system
